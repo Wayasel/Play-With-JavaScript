a9ef26219a4774a8d08e411d9f48dbef404d36d0
Wayasel <md.washeal@gmail.com>
2024-03-17 14:53:23 +0600

    Pig Game: Holding Current Score

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -19,29 +19,52 @@ diceEl.classList.add('hidden');
 let CurrentScore=0;
 let ActivePlayer=0;
 const Scores=[0, 0];
+let Playing = true;
+
+const SwithPlayer= function(){
+    document.getElementById(`current--${ActivePlayer}`).textContent=0;
+    CurrentScore=0;
+    ActivePlayer= ActivePlayer===0 ? 1 : 0;
+    Player0El.classList.toggle('player--active')
+    Player1El.classList.toggle('player--active')
+}
 
 //Rolling Dyce Funcality
 BtnRoll.addEventListener('click', function(){
-    //1. Generate a Random Dice Roll
-    const Dice= Math.trunc(Math.random()* 6)+1;
+    if(Playing){
+        //1. Generate a Random Dice Roll
+        const Dice= Math.trunc(Math.random()* 6)+1;
 
-    //2. Dispaly Dies
-    diceEl.classList.remove('hidden');
-    diceEl.src=`dice-${Dice}.png`;
+        //2. Dispaly Dies
+        diceEl.classList.remove('hidden');
+        diceEl.src=`dice-${Dice}.png`;
 
-    //Check the Roll 1 If True 
-    if(Dice!==1){
-      CurrentScore+=Dice;
-      // Current0El.textContent=CurrentScore;
-      document.getElementById(`current--${ActivePlayer}`).textContent=CurrentScore
-
-    }else{
-    // Switch TO the next Player
-      document.getElementById(`current--${ActivePlayer}`).textContent=0;
-      CurrentScore=0;
-      ActivePlayer= ActivePlayer===0 ? 1 : 0;
-      Player0El.classList.toggle('player--active')
-      Player1El.classList.toggle('player--active')
+        //Check the Roll 1 If True 
+        if(Dice!==1){
+            CurrentScore+=Dice;
+            // Current0El.textContent=CurrentScore;
+            document.getElementById(`current--${ActivePlayer}`).textContent=CurrentScore
+        }
+        else{
+        // Switch TO the next Player
+        SwithPlayer();
+        }
     }
 })
 
+
+BtnHold.addEventListener('click',function(){
+    if(Playing){
+        Scores[ActivePlayer] += CurrentScore;
+        document.getElementById(`score--${ActivePlayer}`).textContent =  Scores[ActivePlayer];
+
+        if(Scores[ActivePlayer]>=20){
+            Playing=false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${ActivePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${ActivePlayer}`).classList.remove('player--active');
+        }else{
+            SwithPlayer();
+        }
+    }
+})
