3b9c3304875114b0d83d9812f98b026990115d0b
Wayasel <md.washeal@gmail.com>
2024-03-17 13:39:30 +0600

    Pig Game: Swithcing The Active Player

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -5,7 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const Current0El=document.getElementById('current--0')
 const Current1El=document.getElementById('current--1')
-
+const Player0El=document.querySelector('.player--0');
+const Player1El=document.querySelector('.player--1');1
 const diceEl = document.querySelector('.dice');
 const BtnNew = document.querySelector('.btn--new');
 const BtnRoll = document.querySelector('.btn--roll');
@@ -16,6 +17,8 @@ score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
 let CurrentScore=0;
+let ActivePlayer=0;
+const Scores=[0, 0];
 
 //Rolling Dyce Funcality
 BtnRoll.addEventListener('click', function(){
@@ -26,16 +29,19 @@ BtnRoll.addEventListener('click', function(){
     diceEl.classList.remove('hidden');
     diceEl.src=`dice-${Dice}.png`;
 
-
     //Check the Roll 1 If True 
     if(Dice!==1){
-        CurrentScore+=Dice;
-        Current0El.textContent=CurrentScore;
+      CurrentScore+=Dice;
+      // Current0El.textContent=CurrentScore;
+      document.getElementById(`current--${ActivePlayer}`).textContent=CurrentScore
 
     }else{
     // Switch TO the next Player
-     
-
+      document.getElementById(`current--${ActivePlayer}`).textContent=0;
+      CurrentScore=0;
+      ActivePlayer= ActivePlayer===0 ? 1 : 0;
+      Player0El.classList.toggle('player--active')
+      Player1El.classList.toggle('player--active')
     }
 })
 
